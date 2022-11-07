9bf5aba51c7e8ead73ce042c801fc2930d95a99f
filipesenaifirjan <filipemattossenai@gmail.com>
2022-11-07 12:05:40 -0300

    cores

--- src/components/Header/styles.js
+++ src/components/Header/styles.js
@@ -29,3 +29,42 @@ display:flex;
 justify-content: center;
 aling-itens: center;
 `
+export const BuscarInputContainer = styled.div`
+width: 175px;
+height: 30px;
+background: #2D2D37;
+border-radius: 8px;
+padding: 2px 5px;
+margin: 0 12px;
+
+`
+export const Menu = styled.a`
+font-family: 'Open Sans',
+font-style: normal;
+line-height: 25px;
+color: #FFFFFF;
+margin-right: 12px;
+test-decoration:nome;
+
+`
+export const MenuRight = styled.a`
+font-family: 'Open Sans',
+font-style: normal;
+line-height: 25px;
+color: #FFFFFF;
+margin-right: 12px;
+test-decoration:nome;
+
+`
+export const UserPicture = styled.ing`
+wigth: 32px;
+height: 32px;
+border-radius: 22px;
+border: 2px solid #FFFFFF;
+`
+export const input = styled.input`
+background: transparent;
+flex:1;
+border: 0;
+color: #FFFFFF;
+`
