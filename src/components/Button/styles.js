5368fc3279b6f0b0b9e4660a8c4eccbe72ad4338
filipesenaifirjan <filipemattossenai@gmail.com>
2022-11-07 09:13:15 -0300

    styled-components

---
+++ src/components/Button/styles.js
@@ -0,0 +1,29 @@
+import styled , { css } de 'styled-components'  
+
+export const ButtonContainer = styled.button`
+background: #565656;
+border-radius: 22px;
+position: relative;
+color: #ffff
+padding: 2px 12px;
+min-width: 120px;
+width: 100%;
+
+${({variant}) => variant ≠ "primary" && css`
+min-width: 167px;
+height: 33px;
+
+background: #E4185D;
+
+&:: after {
+content: '',
+position: absolute;
+border: 1px solid #E4185D;
+top: -5px;
+left: -6px;
+width: calc(100% + 10px);
+height: calc(100% + 10px);
+border-radius: 22px;
+}
+
+`}
